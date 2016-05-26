decd0632c276ac3c269ee198ade8bf841a6c9a46
Sarah Sexton <sarahjeannesexton@live.com>
2016-05-26 04:26:35 -0500

    Added Express

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var port = process.env.PORT || 3000;
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
 var myAppId = process.env.MY_APP_ID || "Missing your app ID";
 var myAppSecret = process.env.MY_APP_SECRET || "Missing your app secret"; 
 
@@ -11,8 +19,12 @@ bot.add('/', function (session) {
 });
 
 // Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+//  var server = restify.createServer();
+//  server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+//  server.listen(process.env.port || 3000, function () {
+//      console.log('%s listening to %s', server.name, server.url);
+//  });
+
+http.listen(port, function(){
+  console.log('listening on ' + port);
 });
