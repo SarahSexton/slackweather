5e9d57a197f217ef4b7491e00fa9e3ad639c53f6
Sarah Sexton <sarahjeannesexton@live.com>
2016-06-04 16:46:17 -0500

    testing bot dialog

--- app.js
+++ app.js
@@ -11,47 +11,47 @@ var weatherKey = process.env.WEATHER_KEY || "Missing weather API key";
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({appId: myAppId, appSecret: myAppSecret });
-// bot.add('/', new builder.SimpleDialog(function (session) { 
-//      session.send('Hello World'); 
-//  })); 
+ bot.add('/', new builder.SimpleDialog(function (session) { 
+      session.send('Hello World'); 
+  })); 
 
-bot.add('/', new builder.CommandDialog()
-.matches('^set location', builder.DialogAction.beginDialog('/profile'))
-.matches('^quit', builder.DialogAction.endDialog())
-.onDefault(function (session) 
-  {
-    if (!session.userData.location) {session.beginDialog('/profile'); } 
-    else {session.send('Location is %s', session.userData.location); }
-    session.send('Hello World');
-  }));
-  bot.add('/profile', [
-    function (session) {
-      builder.Prompts.text(session, 'Hi. Where are you?');
-    },
-    function (session, results) {
-      session.userData.location = results.response;
-      session.endDialog();
-    }
-  ]);
+// bot.add('/', new builder.CommandDialog()
+// .matches('^set location', builder.DialogAction.beginDialog('/profile'))
+// .matches('^quit', builder.DialogAction.endDialog())
+// .onDefault(function (session) 
+//   {
+//     if (!session.userData.location) {session.beginDialog('/profile'); } 
+//     else {session.send('Location is %s', session.userData.location); }
+//     session.send('It is a beautiful day!');
+//   }));
+//   bot.add('/profile', [
+//     function (session) {
+//       builder.Prompts.text(session, 'Hi. Where are you?');
+//     },
+//     function (session, results) {
+//       session.userData.location = results.response;
+//       session.endDialog();
+//     }
+//   ]);
 
 //example of Weather code:
-exports.weather = function(cb) 
-{
-  if (this.message.names) 
-  {
-    var location = this.message.names[0]
-    https.get("https://api.openweathermap.org/data/2.5/find?q=" + location + "&units=imperial" + "&lang=en" + "&mode=json", function(res)
-	{
-		console.log('statusCode: ', res.statusCode);
-		console.log('headers: ', res.headers);
-		res.on('data', function (d) {
-			process.stdout.write(d);
-		});
-	}).on('error', function (e) {
-		console.error(e);
-	});  
-  }
-} 
+// exports.weather = function(cb) 
+// {
+//   if (this.message.names) 
+//   {
+//     var location = this.message.names[0]
+//     https.get("https://api.openweathermap.org/data/2.5/find?q=" + location + "&units=imperial" + "&lang=en" + "&mode=json", function(res)
+// 	{
+// 		console.log('statusCode: ', res.statusCode);
+// 		console.log('headers: ', res.headers);
+// 		res.on('data', function (d) {
+// 			process.stdout.write(d);
+// 		});
+// 	}).on('error', function (e) {
+// 		console.error(e);
+// 	});  
+//   }
+// } 
     // {console.log(response);
     // bot.reply(message, "It's " + response.current_observation.weather + " out right now, and"
     // + ' the temperature is ' + response.current_observation.temp_f + ' degrees F. ' + 
