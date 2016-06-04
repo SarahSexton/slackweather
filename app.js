3f5c4893ac4d1ddd199a44407e33b15fa10e3779
Sarah Sexton <sarahjeannesexton@live.com>
2016-06-04 13:02:26 -0500

    Overhaul

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
-var restify = require('restify');
-var builder = require('botbuilder');
-var server = restify.createServer();
+ var restify = require('restify');
+ var builder = require('botbuilder');
 
 var https = require('https');
 var Weather  = require('openweathermap'); 
 var weatherKey = process.env.WEATHER_KEY || "Missing weather API key"; 
 //var weather = new Weather(weatherKey);
 
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
 var myAppId = process.env.MY_APP_ID || "Missing your app ID";
 var myAppSecret = process.env.MY_APP_SECRET || "Missing your app secret"; 
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: myAppId, appSecret: myAppSecret });
+// bot.add('/', new builder.SimpleDialog(function (session) { 
+//      session.send('Hello World'); 
+//  })); 
+
+
 bot.add('/', new builder.CommandDialog()
 .matches('^set location', builder.DialogAction.beginDialog('/profile'))
 .matches('^quit', builder.DialogAction.endDialog())
 .onDefault(function (session) 
   {
     if (!session.userData.location) {session.beginDialog('/profile'); } 
-    else {session.send('%s', session.userData.location); }
+    else {session.send('Location is %s', session.userData.location); }
     session.send('Hello World');
   }));
   bot.add('/profile', [
@@ -38,11 +34,6 @@ bot.add('/', new builder.CommandDialog()
       session.endDialog();
     }
   ]);
-//bot.listenStdin();
-
-http.listen(port, function(){
-  console.log('listening on ' + port);
-});
 
 //example of Weather code:
 exports.weather = function(cb) 
@@ -54,29 +45,27 @@ exports.weather = function(cb)
 	{
 		console.log('statusCode: ', res.statusCode);
 		console.log('headers: ', res.headers);
-
 		res.on('data', function (d) {
 			process.stdout.write(d);
-			//bot.reply(message, "I heard you. " + d );
 		});
-
 	}).on('error', function (e) {
 		console.error(e);
 	});  
-}
-    
+  }
+} 
     // {console.log(response);
     // bot.reply(message, "It's " + response.current_observation.weather + " out right now, and"
     // + ' the temperature is ' + response.current_observation.temp_f + ' degrees F. ' + 
     // ' It feels like ' + response.current_observation.feelslike_f + ' F out there.');
     // };
  
-
 // Setup Restify Server
-
-  server.use(bot.verifyBotFramework());
+ var server = restify.createServer();
+server.get('/', restify.serveStatic({
+    directory: __dirname,
+    default: '/index.html'
+}));
   server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
   server.listen(process.env.port || 3000, function () {
       console.log('%s listening to %s', server.name, server.url);
   });
-} 
