88f6ae8b5d2e696998b4c9481987bf262fbc85ed
Sarah Sexton <sarahjeannesexton@live.com>
2016-06-13 23:51:53 -0500

    Wunderground Node

--- app.js
+++ app.js
@@ -1,38 +1,81 @@
  var restify = require('restify');
  var builder = require('botbuilder');
+ var Wunderground = require('wundergroundnode');
+ var http = require('http');
 
-var https = require('https');
-var Weather  = require('openweathermap'); 
-var weatherKey = process.env.WEATHER_KEY || "Missing weather API key"; 
-//var weather = new Weather(weatherKey);
+//var https = require('https');
+//var Weather  = require('openweathermap'); 
+//var weatherKey = process.env.WEATHER_KEY || "Missing weather API key"; 
 
- var myAppId = process.env.MY_APP_ID || "Missing your app ID";
- var myAppSecret = process.env.MY_APP_SECRET || "Missing your app secret"; 
+var wundergroundKey = process.env.WUNDERGROUND || "Missing wunderground API key"; 
+var wunderground = new Wunderground(wundergroundKey);
 
-// Create bot and add dialogs
-var bot = new builder.BotConnectorBot({appId: myAppId, appSecret: myAppSecret });
- bot.add('/', new builder.SimpleDialog(function (session) { 
-      session.send('Hello World'); 
-  })); 
+var myAppId = process.env.MY_APP_ID || "Missing your app ID";
+var myAppSecret = process.env.MY_APP_SECRET || "Missing your app secret"; 
 
-// bot.add('/', new builder.CommandDialog()
-// .matches('^set location', builder.DialogAction.beginDialog('/profile'))
-// .matches('^quit', builder.DialogAction.endDialog())
-// .onDefault(function (session) 
-//   {
-//     if (!session.userData.location) {session.beginDialog('/profile'); } 
-//     else {session.send('Location is %s', session.userData.location); }
-//     session.send('It is a beautiful day!');
-//   }));
-//   bot.add('/profile', [
-//     function (session) {
-//       builder.Prompts.text(session, 'Hi. Where are you?');
-//     },
-//     function (session, results) {
-//       session.userData.location = results.response;
-//       session.endDialog();
-//     }
-//   ]);
+////Hello World code
+// var bot = new builder.BotConnectorBot({appId: myAppId, appSecret: myAppSecret });
+// bot.add('/', new builder.SimpleDialog(function(session){session.send('Hello World'); })); 
+
+//Create bot and add dialogs
+ var bot = new builder.BotConnectorBot({ appId: myAppId, appSecret: myAppSecret });
+ bot.add('/', new builder.CommandDialog()
+     .matches('^hi', builder.DialogAction.beginDialog('/hi'))
+     .matches('^weather', builder.DialogAction.beginDialog('/weather'))
+     .onDefault(function (session) {
+         if (!session.userData.location) { session.beginDialog('/weather'); }
+         else { session.send('Hello from %s', session.userData.location + "!"); }
+     }));
+ bot.add('/hi', 
+ [
+     function (session, results) 
+     {
+         if (session.userData.location) { session.beginDialog('/weather'); }
+         else
+         { builder.Prompts.text(session, "Hello. I can tell you about any city if you type, for example, 'weather London, UK'."); }
+     }
+ ]);
+bot.add('/weather', 
+[
+     function (session, results) 
+     {
+        try {
+        var txt = results.matches.input;
+        txt = txt.replace('weather ','');
+        var city = txt.split(',')[0].trim().replace(' ','_');
+        var state = txt.split(',')[1].trim();
+
+        console.log(city + ', ' + state);
+        var url = '/api/'+wundergroundKey+'/conditions/q/state/city.json'
+        url = url.replace('state', state);
+        url = url.replace('city', city);
+
+        http.get(
+            {
+            host: 'api.wunderground.com',
+            path: url
+            }, function(response)
+            {
+                var body = '';
+                response.on('data', function(d) { body += d; })
+                response.on('end', function()
+                {
+                    var data = JSON.parse(body);
+                    var conditions = data.current_observation.weather;
+                    session.send("It appears to be " + conditions + " in "
+                + city + " right now, and the temperature is "
+                + data.current_observation.temp_f + " degrees F.   "
+                + data.current_observation.observation_time);
+                });
+            })
+         }
+         catch (e) {
+        session.send("Whoops, that didn't match! Try again.");
+        s.endDialog({ resumed: dialog.ResumeReason.notCompleted, error: e instanceof Error ? e : new Error(e.toString()) });        
+         }
+    },
+]);
+
 
 //example of Weather code:
 // exports.weather = function(cb) 
@@ -52,19 +95,15 @@ var bot = new builder.BotConnectorBot({appId: myAppId, appSecret: myAppSecret })
 // 	});  
 //   }
 // } 
-    // {console.log(response);
-    // bot.reply(message, "It's " + response.current_observation.weather + " out right now, and"
-    // + ' the temperature is ' + response.current_observation.temp_f + ' degrees F. ' + 
-    // ' It feels like ' + response.current_observation.feelslike_f + ' F out there.');
-    // };
  
 // Setup Restify Server
 var server = restify.createServer();
+server.use(bot.verifyBotFramework({appId: myAppId, appSecret: myAppSecret }));
 server.get('/', restify.serveStatic({
     directory: __dirname,
     default: '/index.html'
 }));
-  server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-  server.listen(process.env.port || 3000, function () {
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3000, function () {
       console.log('%s listening to %s', server.name, server.url);
   });
